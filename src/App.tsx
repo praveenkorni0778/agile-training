196da23d811e39f0f7e9ccb5e80d930758e86a7a
Praveen korni <kornipraveen78@gmail.com>
2024-03-23 11:18:53 +0530

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React aleiw
         </a>
       </header>
     </div>
